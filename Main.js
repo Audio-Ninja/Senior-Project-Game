1f00ca44ea954993d3a9e6387d5cc1c082798a01
Audio-Ninja <123126772+Audio-Ninja@users.noreply.github.com>
2024-02-23 20:42:22 -0700

    Assets

--- Main.js
+++ Main.js
@@ -2,12 +2,44 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 600;
-let testImage = new Image();
-testImage.src = "stuff.png";
+let surfaceImg = new Image();
+surfaceImg.src = "RockSurface.png";
+let surface2Img = new Image();
+surface2Img.src = "RockSurf2.png";
+let botImage = new Image();
+botImage.src = "Robot_Warrior.png";
+let mapRows = 5, mapCols = 10;
+let map = [];
+let iter, clicking = false;
+for(let i = 0; i < mapRows * mapCols; i++) {
+    if(Math.floor(Math.random() * 2) == 0) {
+        map.push(surfaceImg);
+    } else {5
+        map.push(surface2Img);
+    }
+}
 
 function gameLoop() {
     window.requestAnimationFrame(gameLoop);
-    c.drawImage(testImage.image, 0, 0, testImage.image.width, testImage.image.height, testImage.position.x, testImage.position.y, testImage.image.width * testImage.scale, testImage.image.height * testImage.scale)
+    c.fillRect(0,0,canvas.width,canvas.height);
+    iter = 0;
+    for(let i = 0; i < mapRows; i++) {
+        for(let n = 0; n < mapCols; n++) {
+            createImage(map[iter], 10 + 80 * n, 10 + 80 * i);
+            iter++;
+        }
+    }
+}
+
+function createImage(img, x, y) {
+    c.drawImage(img, 0, 0, img.width, img.height, x, y, img.width, img.height);
 }
 
 gameLoop();
+
+window.addEventListener('mousedown', () => {
+    clicking = true;
+})
+window.addEventListener('mouseup', () => {
+    clicking = false;
+})
